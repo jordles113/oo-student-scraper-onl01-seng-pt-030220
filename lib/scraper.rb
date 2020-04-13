9a06a59ee3764fdfb5c258143069785b2dd99651
github username <github email address>
2020-04-13 16:19:39 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,48 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
-  end
+    def self.scrape_index_page(index_url)
+
+      doc = Nokogiri::HTML(open(index_url))
+      student_page = doc.css(".student-card a")
+      students = []
+  
+      student_page.each do |student|
+        students << {
+          name: student.css(".card-text-container h4").text,
+          location: student.css(".card-text-container p").text,
+          profile_url: student.attr('href')
+        }
+      end
+      students
+  end	  
 
   def self.scrape_profile_page(profile_url)
-    
-  end
 
-end
+    doc = Nokogiri::HTML(open(profile_url))
+    student = {}
 
+    student[:profile_quote] = doc.css(".profile-quote").text
+    student[:bio] = doc.css(".description-holder p").text
+
+
+    social_media = doc.css(".social-icon-container a")
+    social_media.each do |social_type|
+      link = social_type.attr('href')
+      if link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      else
+        student[:blog] = link
+      end
+    end
+    student
+  end 
+end 
+ 
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,23 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
-  end
+
+    student_hash.each { |student, value| self.send(("#{student}="), value) }
+    @@all << self
+  end 
 
   def self.create_from_collection(students_array)
-    
+
+    students_array.each { |student| self.new(student) }
   end
 
   def add_student_attributes(attributes_hash)
-    
+
+    attributes_hash.each { |student, value| self.send(("#{student}="), value) }
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
